47e7609256191302f862c7e3ccae1fd3f5d0e6fa
TheRonnieSouza <ronniesouza.contato@gmail.com>
2025-07-07 21:55:50 -0300

    Feat - Factory Pattern with Generics

---
+++ DesignPattern/Factory/MyAIClient/ClientBuilderExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Help.DesignPattern.Factory.MyAIClient
+{
+    public static class ClientBuilderExtension
+    {
+        public static IAIClientBuilder<AIClient, MyAIOptions> AddMeuCliente<TBuilder>(
+         this TBuilder builder, MyAIOptions configuracao)
+         where TBuilder : IAIClientFactoryBuilderWithConfiguration<MyAIOptions>
+        {
+            return builder.RegistrationClient<AIClient>(configuracao);
+        }
+        public static IAIClientBuilder<OutroClienteDeIA, OutrasOpcoesDeIA> AddOutroCliente<TBuilder>(
+       this TBuilder builder, OutrasOpcoesDeIA configuracao)
+       where TBuilder : IAIClientFactoryBuilderWithConfiguration<OutrasOpcoesDeIA>
+        {
+            return builder.RegistrationClient<OutroClienteDeIA>(configuracao);
+        }
+    }
+}
---
+++ DesignPattern/Factory/MyAIClient/Clients.cs
@@ -0,0 +1,38 @@
+﻿namespace Help.DesignPattern.Factory.MyAIClient
+{
+    public class AIClient
+    {
+        public string NomeModelo { get; set; }
+        public double Temperatura { get; set; }
+
+        public void Executar()
+        {
+            Console.WriteLine($"Executando modelo: {NomeModelo} com temperatura: {Temperatura}");
+        }
+    }
+
+    public class  MyAIOptions
+    {
+
+        public string NomeModelo { get; set; }
+        public double Temperatura { get; set; }
+    }
+
+    public class OutroClienteDeIA
+    {
+        public string Endpoint { get; set; }
+        public string ApiKey { get; set; }
+
+        public void Chamar()
+        {
+            Console.WriteLine($"Chamando endpoint: {Endpoint} com API Key: {ApiKey}");
+        }
+    }
+
+    public class OutrasOpcoesDeIA
+    {
+        public string Endpoint { get; set; }
+        public string ApiKey { get; set; }
+    }
+
+}
