3c52ccffe41fb2ee21b6995dfe7a95842e57328c
TheRonnieSouza <ronniesouza.contato@gmail.com>
2025-10-21 20:51:40 -0300

    feat: add solution contains duplicate

---
+++ LeetCode/Arrays/ContainsDuplicate.cs
@@ -0,0 +1,20 @@
+﻿namespace Help.LeetCode.Arrays
+{
+    public class ContainsDuplicate
+    {
+
+        public bool Solution(int[] nums)
+        {
+            var dic = new HashSet<int>();
+
+            foreach (var num in nums)
+            {
+                if (dic.Contains(num))
+                    return true;
+
+                dic.Add(num);
+            }
+            return false;
+        }
+    }
+}
